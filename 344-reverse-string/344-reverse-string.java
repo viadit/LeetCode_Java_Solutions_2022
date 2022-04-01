e7707195d033d51430960a09e957c9292c1229e4
Vishal Aditya <79840941+viadit@users.noreply.github.com>
2022-04-01 21:11:58 +0530

    Time: 1 ms (84.55%), Space: 50.3 MB (81.41%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -2,12 +2,18 @@ class Solution {
     public void reverseString(char[] s) {
         int low = 0;
         int high = s.length - 1;
-        
-        while (low < high) {
-            char temp = s[low];
-            s[low++] = s[high];
-            s[high--] = temp;
+        solve(s, low, high);
+    }
+    
+    public void solve(char[] s, int low, int high) {
+        if (low >= high) {
+            return;
         }
-        return;
+        
+        char temp = s[low];
+        s[low] = s[high];
+        s[high] = temp;
+        
+        solve(s, ++low, --high);
     }
 }
