471f9e400e89614edfdddf435b5ab011e6dcf014
Vishal Aditya <79840941+viadit@users.noreply.github.com>
2022-05-03 18:27:51 +0530

    Time: 58 ms (7.84%), Space: 54.3 MB (31.00%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,27 +1,43 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
-        int low = 0;
-        int n = nums.length;
-        int high = nums.length - 1;
-        int max = 0;
-        int min = nums.length - 1;
+        Stack<Integer> st = new Stack<>();
+        int low = nums.length - 1;
+        int high = 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > nums[max]) {
-                max = i;
-            } else if (nums[max] > nums[i]) {
-                low = i;
-            }
-            if (nums[n - i - 1] < nums[min]) {
-                min = n - i - 1;
-            } else if (nums[n - i - 1] > nums[min]) {
-                high = n - i - 1;
+        for (int i = 0; i < nums.length; ) {
+            if (st.empty()) {
+                st.push(i);
+                i++;
+            } else {
+                if (nums[st.peek()] > nums[i]) {
+                    low = Math.min(low, st.peek());
+                    st.pop();
+                } else {
+                    st.push(i);
+                    i++;
+                }
             }
         }
         
-        if (high >= low) {
+        st.clear();
+        for (int i = nums.length - 1; i >= 0;) {
+            if (st.empty()) {
+                st.push(i);
+                i--;
+            } else {
+                if (nums[st.peek()] < nums[i]) {
+                    high = Math.max(high, st.peek());
+                    st.pop();
+                } else {
+                    st.push(i);
+                    i--;
+                }
+            }
+        }
+        
+        if (low >= high)
             return 0;
-        } else
-            return low - high + 1;
+        else 
+            return high - low + 1;
     }
 }
